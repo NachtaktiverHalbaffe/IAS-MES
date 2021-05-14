81802d33f1c7918b9e1e18822bebb290b491c9ac
NachtaktiverHalbaffe <{e-A17XEv%q7U&>
2021-05-14 17:13:06 +0200

    added Urls to views

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,10 +1,23 @@
 
+from django import urls
 from django.contrib import admin
-from django.urls import path
-from mesapi.views import test_view
+from django.urls import path, include
+from mesapi.views import *
 
 
 urlpatterns = [
+    # url for API authenication
+    path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
-    #path('', test_view, name='test')
+    path('/api/Costumer', CostumerView.as_view()),
+    path('/api/Setting', SettingView.as_view()),
+    path('/api/Error', ErrorView.as_view()),
+    path('/api/WorkingStep', WorkingStepView.as_view()),
+    path('/api/WorkingPlan', WorkingPlanView.as_view()),
+    path('/api/AssignedOrder', AssignedOrderView.as_view()),
+    path('/api/StateWorkingPiece', StateWorkingPieceView.as_view()),
+    path('/api/VisualisationTask', VisualisationTaskView.as_view()),
+    path('/api/StateVisualisationUnit', StateVisualisationUnitView.as_view()),
+    path('/api/StatePLC', StatePLCView.as_view()),
+    path('api/', APIOverview.as_view()),
 ]
