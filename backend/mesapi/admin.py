04aaddfa402eb8a134ef771ed68f0ef617bdfdb3
NachtaktiverHalbaffe <{e-A17XEv%q7U&>
2021-05-14 16:48:26 +0200

    registered models

--- backend/mesapi/admin.py
+++ backend/mesapi/admin.py
@@ -1,3 +1,24 @@
+"""
+Filename: admin.py
+Version name: 0.1, 2021-05-14
+Short description: All models get registered here for the admin panel so they can be managed from it
+
+(C) 2003-2021 IAS, Universitaet Stuttgart
+
+"""
+
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+# Register all the models from models.py
+admin.site.register(StateWorkingPiece)
+admin.site.register(StatePLC)
+admin.site.register(StateVisualisationUnit)
+admin.site.register(AssignedOrder)
+admin.site.register(WorkingPlan)
+admin.site.register(WorkingStep)
+admin.site.register(VisualisationTask)
+admin.site.register(Error)
+admin.site.register(Setting)
