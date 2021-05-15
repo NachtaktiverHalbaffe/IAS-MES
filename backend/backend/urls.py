aaa9fabed815e03bc89f72e1ea99a6c0f19fabc0
NachtaktiverHalbaffe <{e-A17XEv%q7U&>
2021-05-15 13:58:47 +0200

    finished api endpoints

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,4 +1,3 @@
-
 from django import urls
 from django.contrib import admin
 from django.urls import path, include
@@ -7,17 +6,29 @@ from mesapi.views import *
 
 urlpatterns = [
     # url for API authenication
-    path('api-auth/', include('rest_framework.urls')),
-    path('admin/', admin.site.urls),
-    path('api/Costumer/', CostumerView.as_view()),
-    path('api/Setting/', SettingView.as_view()),
-    path('api/Error/', ErrorView.as_view()),
-    path('api/WorkingStep/', WorkingStepView.as_view()),
-    path('api/WorkingPlan/', WorkingPlanView.as_view()),
-    path('api/AssignedOrder/', AssignedOrderView.as_view()),
-    path('api/StateWorkingPiece/', StateWorkingPieceView.as_view()),
-    path('api/VisualisationTask/',  VisualisationTaskView.as_view()),
-    path('api/StateVisualisationUnit/', StateVisualisationUnitView.as_view()),
-    path('api/StatePLC/', StatePLCView.as_view()),
-    path('api/', APIOverview.as_view()),
+    path("api-auth/", include("rest_framework.urls")),
+    # admin panel
+    path("admin/", admin.site.urls),
+    # API urls. Each model has a endpoint for a view for all entities
+    # and a enpoint for a single entity of an object
+    path("api/Costumer/<pk>", SingleCostumerView.as_view()),
+    path("api/Costumer/", CostumerView.as_view()),
+    path("api/Setting/<pk>", SingleSettingView.as_view()),
+    path("api/Setting/", SettingView.as_view()),
+    path("api/Error/<pk>", SingleErrorView.as_view()),
+    path("api/Error/", ErrorView.as_view()),
+    path("api/WorkingStep/<pk>", SingleWorkingStepView.as_view()),
+    path("api/WorkingStep/", WorkingStepView.as_view()),
+    path("api/WorkingPlan/<pk>", SingleWorkingPlanView.as_view()),
+    path("api/WorkingPlan/", WorkingPlanView.as_view()),
+    path("api/AssignedOrder/<pk>", SingleAssignedOrderView.as_view()),
+    path("api/AssignedOrder/", AssignedOrderView.as_view()),
+    path("api/StateWorkingPiece/<pk>", SingleStateWorkingPieceView.as_view()),
+    path("api/StateWorkingPiece/", StateWorkingPieceView.as_view()),
+    path("api/StateVisualisationUnit/<pk>", SingleStateVisualisationUnitView.as_view()),
+    path("api/StateVisualisationUnit/", StateVisualisationUnitView.as_view()),
+    path("api/StatePLC/<pk>", SingleStatePLCView.as_view()),
+    path("api/StatePLC/", StatePLCView.as_view()),
+    # Overview for all endpoints
+    path("api/", APIOverview.as_view()),
 ]
