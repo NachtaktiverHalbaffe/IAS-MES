344926211393333b21a6852e59ff5d8a14904c0d
NachtaktiverHalbaffe <{e-A17XEv%q7U&>
2021-05-18 16:46:14 +0200

    Implemented encodeMessage() in component ServiceOrderHandler

--- backend/mesbackend/serviceorderhandler.py
+++ backend/mesbackend/serviceorderhandler.py
@@ -13,6 +13,7 @@ from .safteymonitoring import SafteyMonitoring
 class ServiceOrderHandler(object):
 
     def __init__(self):
+        self.msg = ""
         # Parameter for service calls
         self.tcpIdent = 0
         self.requestID = 0
@@ -56,7 +57,20 @@ class ServiceOrderHandler(object):
         pass
 
     def decodeMessage(self, msg):
-        pass
+        self.msg = msg
+        if ':' in msg:
+            # msg is in binary format
+            if "33:33:33:01:" in msg:
+                self.tcpIdent = "33:33:33:01:"
+            elif "33:33:33:02:" in msg:
+                self.tcpIdent = "33:33:33:02:"
+            self._decodeBin()
+        elif '445' in msg:
+            # msg is in shortened string format
+            self._decodeStrShort()
+        elif '444' in msg:
+            # msg is in full string format
+            self._decodeStrFull()
 
     def getOutputParams(self):
         pass
@@ -85,7 +99,7 @@ class ServiceOrderHandler(object):
     # Format is parameter=value and each parameter is seperated with a ";"
     # @params: Takes all the neccessary attributes of the Object and parses them
     def _encodeStrFull(self):
-        #Header
+        # Header
         msg = str(self.tcpIdent)
         if self.requestID != 0:
             msg += ";RequestID=" + str(self.requestID)
@@ -156,11 +170,89 @@ class ServiceOrderHandler(object):
 
         # service specific paramter. Each parameter is a 2 tuple with (parametername, parameter)
         for item in self.serviceParams:
-            msg += ";" + item[0] + "=" + str(item[1])
+            msg += ";" + item[0] + "=" + item[1]
 
         # evry message ends with <CR>
         msg += "<CR>"
         return msg
+
+    def _decodeStrFull(self):
+        # split parameter
+        msg = self.msg.split(";")
+        # saving all parameter into object
+        for item in msg:
+            # Header
+            param = item.split("=")
+            if '444' in item:
+                self.tcpIdent = int(item)
+            elif 'RequestID' in item:
+                self.requestID = param[1]
+            elif 'MClass' in item:
+                self.mClass = param[1]
+            elif 'MNo' in item:
+                self.mNo = param[1]
+            elif 'ErrorState' in item:
+                self.errorState = param[1]
+            elif 'DataLength' in item:
+                self.dataLength = param[1]
+
+            # standardparameter
+            elif 'ResourceID' in item:
+                self.resourceId = param[1]
+            elif 'ONo' in item:
+                self.oNo = param[1]
+            elif 'OPos' in item:
+                self.oPos = param[1]
+            elif 'wpNo' in item:
+                self.wpNo = param[1]
+            elif 'OpNo' in item:
+                self.opNo = param[1]
+            elif 'BufNo' in item:
+                self.bufNo = param[1]
+            elif 'BufPos' in item:
+                self.bufPos = param[1]
+            elif 'CarrierID' in item:
+                self.carrierId = param[1]
+            elif 'PalletID' in item:
+                self.palletID = param[1]
+            elif 'PalletPos' in item:
+                self.palletPos = param[1]
+            elif 'PNo' in item:
+                self.pNo = param[1]
+            elif 'StopperID' in item:
+                self.stopperId = param[1]
+            elif 'ErrorStepNo' in item:
+                self.errorStepNo = param[1]
+            elif 'StepNo' in item:
+                self.stepNo = param[1]
+            elif 'MaxRecords' in item:
+                self.maxRecords = param[1]
+            elif 'BoxID' in item:
+                self.boxId = param[1]
+            elif 'BoxPos' in item:
+                self.boxPos = param[1]
+            elif 'MainOPos' in item:
+                self.mainOPos = param[1]
+            elif 'BeltNo' in item:
+                self.beltNo = param[1]
+            elif 'CNo' in item:
+                self.cNo = param[1]
+            elif 'BoxPNo' in item:
+                self.boxPNo = param[1]
+            elif 'PalletPNo=' in item:
+                self.palletPNo = param[1]
+            elif 'Aux1Int' in item:
+                self.aux1Int = param[1]
+            elif 'Aux2Int' in item:
+                self.aux2Int = param[1]
+            elif 'Aux1DInt' in item:
+                self.aux1DInt = param[1]
+            elif 'Aux2DInt' in item:
+                self.aux2DInt = param[1]
+            elif 'MainPNo' in item:
+                self.mainPNo = param[1]
+            else:
+                self.serviceParams.append((param[0], param[1]))
 
     # encodes message with shortemd string format. Like the binary format evry parameter is send in
     # the right order in ASCII and every parameter is seperated with a "<CR>"
@@ -208,43 +300,46 @@ class ServiceOrderHandler(object):
             msg += str(item) + "<CR>"
         return msg
 
+    def _decodeStrShort(self):
+        pass
+
     def _encodeBin(self):
         # Header
         msg = str(self.tcpIdent)
-        msg += self._parseEndian(self.requestID, False)
-        msg += self._parseEndian(self.mClass, False)
-        msg += self._parseEndian(self.mNo, False)
-        msg += self._parseEndian(self.errorState, False)
-        msg += self._parseEndian(self.dataLength, False)
+        msg += self._parseToEndian(self.requestID, False)
+        msg += self._parseToEndian(self.mClass, False)
+        msg += self._parseToEndian(self.mNo, False)
+        msg += self._parseToEndian(self.errorState, False)
+        msg += self._parseToEndian(self.dataLength, False)
 
         # standardparameter
-        msg += self._parseEndian(self.resourceId, False)
-        msg += self._parseEndian(self.oNo, True)
-        msg += self._parseEndian(self.oPos, False)
-        msg += self._parseEndian(self.wpNo, False)
-        msg += self._parseEndian(self.opNo, False)
-        msg += self._parseEndian(self.bufNo, False)
-        msg += self._parseEndian(self.bufPos, False)
-        msg += self._parseEndian(self.carrierId, False)
-        msg += self._parseEndian(self.palletID, False)
-        msg += self._parseEndian(self.palletPos, False)
-        msg += self._parseEndian(self.pNo, True)
-        msg += self._parseEndian(self.stopperId, False)
-        msg += self._parseEndian(self.errorStepNo, False)
-        msg += self._parseEndian(self.stepNo, False)
-        msg += self._parseEndian(self.maxRecords, False)
-        msg += self._parseEndian(self.boxId, False)
-        msg += self._parseEndian(self.boxPos, False)
-        msg += self._parseEndian(self.mainOPos, False)
-        msg += self._parseEndian(self.beltNo, False)
-        msg += self._parseEndian(self.cNo, True)
-        msg += self._parseEndian(self.boxPNo, True)
-        msg += self._parseEndian(self.palletPNo, True)
-        msg += self._parseEndian(self.aux1Int, False)
-        msg += self._parseEndian(self.aux2Int, False)
-        msg += self._parseEndian(self.aux1DInt, True)
-        msg += self._parseEndian(self.aux2DInt, True)
-        msg += self._parseEndian(self.mainPNo, True)
+        msg += self._parseToEndian(self.resourceId, False)
+        msg += self._parseToEndian(self.oNo, True)
+        msg += self._parseToEndian(self.oPos, False)
+        msg += self._parseToEndian(self.wpNo, False)
+        msg += self._parseToEndian(self.opNo, False)
+        msg += self._parseToEndian(self.bufNo, False)
+        msg += self._parseToEndian(self.bufPos, False)
+        msg += self._parseToEndian(self.carrierId, False)
+        msg += self._parseToEndian(self.palletID, False)
+        msg += self._parseToEndian(self.palletPos, False)
+        msg += self._parseToEndian(self.pNo, True)
+        msg += self._parseToEndian(self.stopperId, False)
+        msg += self._parseToEndian(self.errorStepNo, False)
+        msg += self._parseToEndian(self.stepNo, False)
+        msg += self._parseToEndian(self.maxRecords, False)
+        msg += self._parseToEndian(self.boxId, False)
+        msg += self._parseToEndian(self.boxPos, False)
+        msg += self._parseToEndian(self.mainOPos, False)
+        msg += self._parseToEndian(self.beltNo, False)
+        msg += self._parseToEndian(self.cNo, True)
+        msg += self._parseToEndian(self.boxPNo, True)
+        msg += self._parseToEndian(self.palletPNo, True)
+        msg += self._parseToEndian(self.aux1Int, False)
+        msg += self._parseToEndian(self.aux2Int, False)
+        msg += self._parseToEndian(self.aux1DInt, True)
+        msg += self._parseToEndian(self.aux2DInt, True)
+        msg += self._parseToEndian(self.mainPNo, True)
         # standardparameter bytes reserved
         for item in range(44):
             msg += "00:"
@@ -252,19 +347,64 @@ class ServiceOrderHandler(object):
         # servicespecific parameter
         for i in range(len(self.serviceParams)):
             if i != len(self.serviceParams):
-                msg += self._parseEndian(self.serviceParams[i], False) + ":"
+                msg += self._parseToEndian(self.serviceParams[i], False) + ":"
             else:
-                msg += self._parseEndian(self.serviceParams[i], False)
+                msg += self._parseToEndian(self.serviceParams[i], False)
                 # cut of last ":"
                 msg = msg[:len(msg)-1]
 
         return msg
 
+    def _decodeBin(self):
+        # header
+        bytes = self.msg.split(":")
+        self.requestID = self._parseFromEndian(bytes[4:6])
+        self.mClass = self._parseFromEndian(bytes[6:8])
+        self.mNo = self._parseFromEndian(bytes[8:10])
+        self.errorState = self._parseFromEndian(bytes[10:12])
+        self.dataLength = self._parseFromEndian(bytes[12:14])
+
+        # standard parameter
+        self.resourceId = self._parseFromEndian(bytes[14:16])
+        self.oNo = self._parseFromEndian(bytes[16:20])
+        self.oPos = self._parseFromEndian(bytes[20:22])
+        self.wpNo = self._parseFromEndian(bytes[22:24])
+        self.opNo = self._parseFromEndian(bytes[24:26])
+        self.bufNo = self._parseFromEndian(bytes[26:28])
+        self.bufPos = self._parseFromEndian(bytes[28:30])
+        self.carrierId = self._parseFromEndian(bytes[30:32])
+        self.palletID = self._parseFromEndian(bytes[34:36])
+        self.palletPos = self._parseFromEndian(bytes[36:38])
+        self.pNo = self._parseFromEndian(bytes[38:42])
+        self.stopperId = self._parseFromEndian(bytes[42:44])
+        self.errorStepNo = self._parseFromEndian(bytes[44:46])
+        self.stepNo = self._parseFromEndian(bytes[46:48])
+        self.maxRecords = self._parseFromEndian(bytes[48:50])
+        self.boxId = self._parseFromEndian(bytes[50:52])
+        self.boxPos = self._parseFromEndian(bytes[52:54])
+        self.mainOPos = self._parseFromEndian(bytes[54:56])
+        self.beltNo = self._parseFromEndian(bytes[56:58])
+        self.cNo = self._parseFromEndian(bytes[58:62])
+        self.boxPNo = self._parseFromEndian(bytes[62:66])
+        self.palletPNo = self._parseFromEndian(bytes[66:70])
+        self.aux1Int = self._parseFromEndian(bytes[70:72])
+        self.aux2Int = self._parseFromEndian(bytes[72:72])
+        self.aux1DInt = self._parseFromEndian(bytes[72:76])
+        self.aux2DInt = self._parseFromEndian(bytes[76:80])
+        self.mainPNo = self._parseFromEndian(bytes[80:84])
+
+        # service-specific parameter
+        if len(bytes) != 128:
+            for i in range(128, len(bytes), 2):
+                self.serviceParams.append(
+                    self._parseFromEndian(bytes[128+i, 130+i]))
+
     # parses a number to hex in the binary format
     # @params:
     # number: number to parse
     # isInt32: if number is int32 (true) or int16(false)
-    def _parseEndian(self, number, isInt32):
+
+    def _parseToEndian(self, number, isInt32):
         if isInt32:
             hex = format(number, "08x")
         else:
@@ -281,3 +421,17 @@ class ServiceOrderHandler(object):
                 binStr += binArray[i] + ":"
 
         return binStr
+
+    # parses given bytes to number depending if message is in big or little endian
+    # @params:
+    # bytes: bytes to parse
+    def _parseFromEndian(self, bytes):
+        nmbrstr = ""
+        if self.tcpIdent == "33:33:33:02:":
+            for i in range(0, len(bytes), 1):
+                nmbrstr += bytes[i]
+        elif self.tcpIdent == "33:33:33:01:":
+            for i in range(len(bytes)-1, -1, -1):
+                nmbrstr += bytes[i]
+
+        return int(nmbrstr, 16)
