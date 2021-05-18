25adfed0ae5af1bd87903d2c54c5348ed8a17d07
NachtaktiverHalbaffe <{e-A17XEv%q7U&>
2021-05-18 17:35:14 +0200

    Created module Servicecalls to ourtsource servicecalls

---
+++ backend/mesbackend/servicecalls.py
@@ -0,0 +1,44 @@
+"""
+Filename: servicecalls.py
+Version name: 0.1, 2021-05-18
+Short description: Module for handling the service calls. The buisness logic for each servicecall 
+is handled here. Given on the servicecalls some output parameters are set
+
+(C) 2003-2021 IAS, Universitaet Stuttgart
+
+"""
+
+from .systemmonitoring import SystemMonitoring
+
+
+class Servicecalls(object):
+
+    def __init__(self):
+        self.systemmonitoring = SystemMonitoring()
+
+    def getFirstOpForRsc(self, obj):
+        return
+
+    def getOpForONoOPos(self, obj):
+        return
+
+    def opStart(self, obj):
+        return
+
+    def opReset(self, obj):
+        return
+
+    def opEnd(self, obj):
+        return
+
+    def getShuntForTarget(self, obj):
+        return
+
+    def getBufForBufNo(self, obj):
+        return
+
+    def getBufPos(self, obj):
+        return
+
+    def getToAGVBuf(self, obj):
+        return
