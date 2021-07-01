54a6704ca9274c204aa6ea7801c16cab98e94137
NachtaktiverHalbaffe <{e-A17XEv%q7U&>
2021-07-01 21:14:41 +0200

    improved validating workingplan (checks also states)

--- frontend/src/components/validateworkingsteps/validateworkingsteps.js
+++ frontend/src/components/validateworkingsteps/validateworkingsteps.js
@@ -9,6 +9,11 @@ Short description: Page to display all errors
 
 export default function validateWorkingsteps(workingsteps) {
   let steps = workingsteps.sort((a, b) => (a.stepNo > b.stepNo ? 1 : -1));
+  let state = {
+    isAssembled: null,
+    isPackaged: null,
+    isStored: null,
+  };
   let errormsg = "";
   let isValid = true;
   if (steps.length !== 0) {
@@ -16,51 +21,59 @@ export default function validateWorkingsteps(workingsteps) {
       if (i === 0 && steps[i]["task"] !== "unstore") {
         return [false, "First workingstep must be unstore"];
       } else if (steps[i]["task"] === "unstore") {
-        let validator = mCheckUnstore(steps, isValid, i);
+        let validator = mCheckUnstore(steps, i + 1, state);
         isValid = validator[0];
         errormsg = validator[1];
+        state = validator[2];
         if (!isValid) {
           return [false, errormsg];
         }
       } else if (steps[i]["task"] === "store") {
-        let validator = mCheckStore(steps, isValid, i);
+        let validator = mCheckStore(steps, i + 1, state);
         isValid = validator[0];
         errormsg = validator[1];
+        state = validator[2];
         if (!isValid) {
           return [false, errormsg];
         }
       } else if (steps[i]["task"] === "package") {
-        let validator = mCheckPackage(steps, isValid, i);
+        let validator = mCheckPackage(steps, i + 1, state);
         isValid = validator[0];
         errormsg = validator[1];
+        state = validator[2];
         if (!isValid) {
           return [false, errormsg];
         }
       } else if (steps[i]["task"] === "unpackage") {
-        let validator = mCheckUnpackage(steps, isValid, i);
+        let validator = mCheckUnpackage(steps, i + 1, state);
+        console.log(validator);
         isValid = validator[0];
         errormsg = validator[1];
+        state = validator[2];
         if (!isValid) {
           return [false, errormsg];
         }
       } else if (steps[i]["task"] === "assemble") {
-        let validator = mCheckAssemble(steps, isValid, i);
+        let validator = mCheckAssemble(steps, i + 1, state);
         isValid = validator[0];
         errormsg = validator[1];
+        state = validator[2];
         if (!isValid) {
           return [false, errormsg];
         }
       } else if (steps[i]["task"] === "color") {
-        let validator = mCheckColor(steps, isValid, i);
+        let validator = mCheckColor(steps, i + 1, state);
         isValid = validator[0];
         errormsg = validator[1];
+        state = validator[2];
         if (!isValid) {
           return [false, errormsg];
         }
       } else if (steps[i]["task"] === "generic") {
-        let validator = mCheckGeneric(steps, isValid, i);
+        let validator = mCheckGeneric(steps, i + 1, state);
         isValid = validator[0];
         errormsg = validator[1];
+        state = validator[2];
         if (!isValid) {
           return [false, errormsg];
         }
@@ -71,142 +84,268 @@ export default function validateWorkingsteps(workingsteps) {
   return [true, errormsg];
 }
 
-function mCheckUnstore(workingsteps, isValid, range) {
-  let mIsValid = isValid;
+function mCheckUnstore(workingsteps, range, state) {
+  let mIsValid = true;
   let errormsg = "";
+  let newState = state;
   for (let i = 0; i < range; i++) {
     if (i !== 0) {
+      // validating
       if (workingsteps[i]["task"] === "store") {
+        newState["isStored"] = true;
+        mIsValid = true;
+        errormsg = "";
+      } else if (workingsteps[i]["task"] === "unstore") {
+        if (newState["isStored"] === true) {
+          mIsValid = true;
+          newState["isStored"] = false;
+          errormsg = "";
+        } else {
+          mIsValid = false;
+          errormsg = "Workingpiece must be stored before being unstored";
+        }
+      }
+      // state tracking
+      else if (workingsteps[i]["task"] === "assemble") {
+        newState["isAssembled"] = true;
+      } else if (workingsteps[i]["task"] === "generic") {
+        newState["isAssembled"] = false;
+      } else if (workingsteps[i]["task"] === "package") {
+        newState["isPackaged"] = true;
+      } else if (workingsteps[i]["task"] === "unpackage") {
+        newState["isPackaged"] = false;
+      }
+    }
+  }
+  return [mIsValid, errormsg, newState];
+}
+
+function mCheckStore(workingsteps, range, state) {
+  let mIsValid = true;
+  let errormsg = "";
+  let newState = state;
+  for (let i = 0; i < range; i++) {
+    // validating
+    if (workingsteps[i]["task"] === "store") {
+      if (newState["isStored"] == true) {
+        mIsValid = false;
+        errormsg = "Workingpiece must be unstored before being stored";
+      }
+      newState["isStored"] = true;
+    } else if (workingsteps[i]["task"] === "unstore") {
+      if (newState["isStored"] == true) {
         mIsValid = true;
         errormsg = "";
       }
-      if (workingsteps[i]["task"] === "unstore") {
-        mIsValid = false;
-        errormsg = "Workingpiece must be stored before being unstored";
-      }
-    }
-  }
-  return [mIsValid, errormsg];
-}
-
-function mCheckStore(workingsteps, isValid, range) {
-  let mIsValid = isValid;
-  let errormsg = "";
-  for (let i = 0; i < range; i++) {
-    if (workingsteps[i]["task"] === "store") {
-      mIsValid = false;
-      errormsg = "Workingpiece must be unstored before being stored";
-    }
-    if (workingsteps[i]["task"] === "unstore") {
-      mIsValid = true;
-      errormsg = "";
-    }
-  }
-  return [mIsValid, errormsg];
-}
-
-function mCheckPackage(workingsteps, isValid, range) {
-  let mIsValid = isValid;
-  let errormsg = "";
-  for (let i = 0; i < range; i++) {
-    if (workingsteps[i]["task"] === "unpackage") {
-      mIsValid = true;
-      errormsg = "";
-    }
-    if (workingsteps[i]["task"] === "package") {
-      mIsValid = false;
-      errormsg = "Workingpiece must be unpackaged before being packaged";
-    }
-    if (workingsteps[i]["task"] === "unstore") {
-      mIsValid = true;
-      errormsg = "";
-    }
-    if (workingsteps[i]["task"] === "store") {
-      mIsValid = false;
-      errormsg = "Workingpiece must be unstored before being packaged";
-    }
-  }
-  return [mIsValid, errormsg];
-}
-
-function mCheckUnpackage(workingsteps, isValid, range) {
-  let mIsValid = false;
-  let errormsg = "Workingpiece must be packaged before being unpackaged";
-  for (let i = 0; i < range; i++) {
-    if (workingsteps[i]["task"] === "unpackage") {
-      mIsValid = false;
-      errormsg = "Workingpiece must be packaged before being unpackaged";
-    }
-    if (workingsteps[i]["task"] === "package") {
-      mIsValid = true;
-      errormsg = "";
-    }
-    if (workingsteps[i]["task"] === "store") {
-      mIsValid = false;
-      errormsg = "Workingpiece must be unstored before being packaged";
-    }
-  }
-  return [mIsValid, errormsg];
-}
-
-function mCheckAssemble(workingsteps, isValid, range) {
-  let mIsValid = isValid;
-  let errormsg = "";
-  for (let i = 0; i < range; i++) {
-    if (workingsteps[i]["task"] === "unpackage") {
-      mIsValid = true;
-      errormsg = "";
-    } else if (workingsteps[i]["task"] === "package") {
-      mIsValid = false;
-      errormsg = "Workingpiece must be unpackaged before being assembled";
-    } else if (workingsteps[i]["task"] === "store") {
-      mIsValid = false;
-      errormsg = "Workingpiece must be unstored before being assembled";
+      newState["isStored"] = false;
+      // state tracking
     } else if (workingsteps[i]["task"] === "assemble") {
-      mIsValid = false;
-      errormsg = "Workingpiece must be unassembled before being assembled";
+      newState["isAssembled"] = true;
+    } else if (workingsteps[i]["task"] === "generic") {
+      newState["isAssembled"] = false;
+    } else if (workingsteps[i]["task"] === "package") {
+      newState["isPackaged"] = true;
+    } else if (workingsteps[i]["task"] === "unpackage") {
+      newState["isPackaged"] = false;
     }
   }
-  return [mIsValid, errormsg];
+  return [mIsValid, errormsg, newState];
 }
 
-function mCheckColor(workingsteps, isValid, range) {
-  let mIsValid = isValid;
+function mCheckPackage(workingsteps, range, state) {
+  let mIsValid = true;
   let errormsg = "";
+  let newState = state;
   for (let i = 0; i < range; i++) {
+    // validating
     if (workingsteps[i]["task"] === "unpackage") {
-      mIsValid = true;
-      errormsg = "";
-    }
-    if (workingsteps[i]["task"] === "package") {
-      mIsValid = false;
-      errormsg = "Workingpiece must be unpackaged before being painted";
-    }
-    if (workingsteps[i]["task"] === "store") {
-      mIsValid = false;
-      errormsg = "Workingpiece must be unstored before being painted";
-    }
-  }
-  return [mIsValid, errormsg];
-}
-
-function mCheckGeneric(workingsteps, isValid, range) {
-  let mIsValid = isValid;
-  let errormsg = "";
-  for (let i = 0; i < range; i++) {
-    if (workingsteps[i]["task"] === "unpackage") {
-      mIsValid = true;
-      errormsg = "";
-    }
-    if (workingsteps[i]["task"] === "package") {
-      mIsValid = false;
-      errormsg =
-        "Workingpiece must be unpackaged before executing generic task";
-    }
-    if (workingsteps[i]["task"] === "store") {
+      if (newState["isPackaged"] == true) {
+        mIsValid = true;
+        errormsg = "";
+      }
+      newState["isPackaged"] = false;
+    } else if (workingsteps[i]["task"] === "package") {
+      if (newState["isPackaged"] == true) {
+        mIsValid = false;
+        errormsg = "Workingpiece must be unpackaged before being packaged";
+      }
+      newState["isPackaged"] = true;
+    } else if (workingsteps[i]["task"] === "unstore") {
+      if (newState["isStored"] == true) {
+        mIsValid = true;
+        errormsg = "";
+      }
+      newState["isStored"] = false;
+    } else if (workingsteps[i]["task"] === "store") {
+      newState["isStored"] = true;
       mIsValid = false;
       errormsg = "Workingpiece must be unstored before executing generic task";
     }
+    // state tracking
+    else if (workingsteps[i]["task"] === "assemble") {
+      newState["isAssembled"] = true;
+    } else if (workingsteps[i]["task"] === "generic") {
+      newState["isAssembled"] = false;
+    }
   }
-  return [mIsValid, errormsg];
+  return [mIsValid, errormsg, newState];
+}
+
+function mCheckUnpackage(workingsteps, range, state) {
+  let mIsValid = true;
+  let errormsg = "";
+  let newState = state;
+  for (let i = 0; i < range; i++) {
+    // validating
+    if (workingsteps[i]["task"] === "unpackage") {
+      if (newState["isPackaged"] == false) {
+        mIsValid = false;
+        errormsg = "Workingpiece must be packaged before being unpackaged";
+      }
+      newState["isPackaged"] = false;
+    } else if (workingsteps[i]["task"] === "package") {
+      if (newState["isPackaged"] == false) {
+        mIsValid = true;
+        errormsg = "";
+      }
+      newState["isPackaged"] = true;
+    } else if (workingsteps[i]["task"] === "store") {
+      newState["isStored"] = true;
+      mIsValid = false;
+      errormsg = "Workingpiece must be unstored before executing generic task";
+    } else if (workingsteps[i]["task"] === "unstore") {
+      if (newState["isStored"] == true) {
+        newState["isStored"] = false;
+        mIsValid = true;
+        errormsg = "";
+      }
+    }
+    // state tracking
+    else if (workingsteps[i]["task"] === "assemble") {
+      newState["isAssembled"] = true;
+    } else if (workingsteps[i]["task"] === "generic") {
+      newState["isAssembled"] = false;
+    }
+  }
+  return [mIsValid, errormsg, newState];
+}
+
+function mCheckAssemble(workingsteps, range, state) {
+  let mIsValid = true;
+  let errormsg = "";
+  let newState = state;
+  for (let i = 0; i < range; i++) {
+    // validating
+    if (workingsteps[i]["task"] === "unpackage") {
+      if (newState["isPackaged"] == true) {
+        mIsValid = true;
+        errormsg = "";
+      }
+      newState["isPackaged"] = false;
+    } else if (workingsteps[i]["task"] === "package") {
+      if (newState["isPackaged"] == false) {
+        mIsValid = false;
+        errormsg = "Workingpiece must be unpackaged before executing assemble";
+      }
+      newState["isPackaged"] = true;
+    } else if (workingsteps[i]["task"] === "store") {
+      newState["isStored"] = true;
+      mIsValid = false;
+      errormsg = "Workingpiece must be unstored before executing assemble";
+    } else if (workingsteps[i]["task"] === "unstore") {
+      if (newState["isStored"] == true) {
+        mIsValid = true;
+        errormsg = "";
+      }
+      newState["isStored"] = false;
+    } else if (workingsteps[i]["task"] === "assemble") {
+      if (newState["isAssembled"] == true) {
+        mIsValid = false;
+        errormsg = "Workingpiece must be unassembled before being assembled";
+      }
+      newState["isAssembled"] = true;
+    } else if (workingsteps[i]["task"] === "generic") {
+      if (newState["isAssembled"] == true) {
+        mIsValid = true;
+        errormsg = "";
+      }
+      newState["isAssembled"] = false;
+    }
+  }
+  return [mIsValid, errormsg, newState];
+}
+
+function mCheckColor(workingsteps, range, state) {
+  let mIsValid = true;
+  let errormsg = "";
+  let newState = state;
+  for (let i = 0; i < range; i++) {
+    // validating
+    if (workingsteps[i]["task"] === "unpackage") {
+      if (newState["isPackaged"] == true) {
+        mIsValid = true;
+        errormsg = "";
+      }
+      newState["isPackaged"] = false;
+    } else if (workingsteps[i]["task"] === "package") {
+      mIsValid = false;
+      newState["isPackaged"] = true;
+      errormsg = "Workingpiece must be unpackaged before being painted";
+    } else if (workingsteps[i]["task"] === "store") {
+      newState["isStored"] = true;
+      mIsValid = false;
+      errormsg = "Workingpiece must be unstored before executing generic task";
+    } else if (workingsteps[i]["task"] === "unstore") {
+      if (newState["isStored"] == true) {
+        mIsValid = true;
+        errormsg = "";
+      }
+      newState["isStored"] = false;
+    }
+    // state tracking
+    else if (workingsteps[i]["task"] === "assemble") {
+      newState["isAssembled"] = true;
+    } else if (workingsteps[i]["task"] === "genric") {
+      newState["isAssembled"] = false;
+    }
+  }
+  return [mIsValid, errormsg, newState];
+}
+
+function mCheckGeneric(workingsteps, range, state) {
+  let mIsValid = true;
+  let errormsg = "";
+  let newState = state;
+  for (let i = 0; i < range; i++) {
+    // validating
+    if (workingsteps[i]["task"] === "unpackage") {
+      if (newState["isPackaged"] == true) {
+        mIsValid = true;
+        errormsg = "";
+      }
+      newState["isPackaged"] = false;
+    } else if (workingsteps[i]["task"] === "package") {
+      newState["isPackaged"] = true;
+      mIsValid = false;
+      errormsg =
+        "Workingpiece must be unpackaged before executing generic task";
+    } else if (workingsteps[i]["task"] === "store") {
+      newState["isStored"] = true;
+      mIsValid = false;
+      errormsg = "Workingpiece must be unstored before executing generic task";
+    } else if (workingsteps[i]["task"] === "unstore") {
+      if (newState["isStored"] == true) {
+        mIsValid = true;
+        errormsg = "";
+      }
+      newState["isStored"] = false;
+    }
+    // state tracking
+    else if (workingsteps[i]["task"] === "assemble") {
+      newState["isAssembled"] = true;
+    } else if (workingsteps[i]["task"] === "generic") {
+      newState["isAssembled"] = true;
+    }
+  }
+  return [mIsValid, errormsg, newState];
 }
